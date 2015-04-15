cb72c8830c67602d6035b2120b8d464f9ca8b163
nomantufail <nomantufail100@gmail.com>
2015-04-15 03:04:49 -0700

    new change

--- application/models/stock_model.php
+++ application/models/stock_model.php
@@ -50,10 +50,10 @@ class Stock_Model extends Parent_Model {
             return null;
         }
     }
-    public function increase($invoice_entries)
+    public function increase($stock_entries)
     {
         $product_ids = array();
-        foreach($invoice_entries as $entry)
+        foreach($stock_entries as $entry)
         {
             array_push($product_ids, $entry['product_id']);
         }
@@ -66,26 +66,25 @@ class Stock_Model extends Parent_Model {
             $this->db->where_in("product_id", $product_ids);
             $result = $this->db->get($this->table)->result();
 
-            $modified_invoice_entries = array();
+            $modified_stock_entries = array();
             foreach($result as $record)
             {
-                foreach($invoice_entries as $entry)
+                foreach($stock_entries as $entry)
                 {
                     if($entry['product_id'] == $record->product_id)
                     {
-                        $modified_invoice_entry = array(
+                        $modified_stock_entry = array(
                             'product_id' => $entry['product_id'],
                             'quantity' => $entry['quantity'] + $record->quantity,
                             'updated_at' => date('m/d/Y h:i:s', time()),
                         );
-                        array_push($modified_invoice_entries, $modified_invoice_entry);
+                        array_push($modified_stock_entries, $modified_stock_entry);
                     }
                 }
             }
-
-            if(sizeof($modified_invoice_entries) > 0)
+            if(sizeof($modified_stock_entries) > 0)
             {
-                $this->db->update_batch($this->table, $modified_invoice_entries, 'product_id');
+                $this->db->update_batch($this->table, $modified_stock_entries, 'product_id');
             }
 
             return $this->db->trans_complete();
@@ -94,10 +93,10 @@ class Stock_Model extends Parent_Model {
 
 
     }
-    public function decrease($invoice_entries)
+    public function decrease($stock_entries)
     {
         $product_ids = array();
-        foreach($invoice_entries as $entry)
+        foreach($stock_entries as $entry)
         {
             array_push($product_ids, $entry['product_id']);
         }
@@ -110,27 +109,26 @@ class Stock_Model extends Parent_Model {
             $this->db->where_in("product_id", $product_ids);
             $result = $this->db->get($this->table)->result();
 
-            $modified_invoice_entries = array();
+            $modified_stock_entries = array();
             foreach($result as $record)
             {
-                foreach($invoice_entries as $entry)
+                foreach($stock_entries as $entry)
                 {
                     if($entry['product_id'] == $record->product_id)
                     {
-                        $modified_invoice_entry = array(
+                        $modified_stock_entry = array(
                             'product_id' => $entry['product_id'],
-                            'quantity' => $record->quantity - $entry['quantity'],
-                            'updated_at' => date('Y-m-d h:i:s', time()),
+                            'quantity' => $entry['quantity'] - $record->quantity,
+                            'updated_at' => date('m/d/Y h:i:s', time()),
                         );
-                        array_push($modified_invoice_entries, $modified_invoice_entry);
+                        array_push($modified_stock_entries, $modified_stock_entry);
                     }
                 }
             }
 
-            //var_dump($modified_invoice_entries); die();
-            if(sizeof($modified_invoice_entries) > 0)
+            if(sizeof($modified_stock_entries) > 0)
             {
-                $this->db->update_batch($this->table, $modified_invoice_entries, 'product_id');
+                $this->db->update_batch($this->table, $modified_stock_entries, 'product_id');
             }
 
             return $this->db->trans_complete();
